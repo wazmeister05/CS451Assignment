0c3b62db908d5b2755d626eb9398e7d60ae36012
GA <w.stolton@protonmail.com>
2022-02-13 02:40:39 +0000

    trying to remove "package" declaration

--- CS451TestSystem/weblog-analyzer/LogEntry.java
+++ CS451TestSystem/weblog-analyzer/LogEntry.java
@@ -1,4 +1,4 @@
-package weblog
+package weblog;
 
 import java.util.Calendar;
 
--- src/WMCSimple.java
+++ src/WMCSimple.java
@@ -1,35 +1,62 @@
-import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.ParserCollectionStrategy;
+import com.github.javaparser.utils.ProjectRoot;
+import com.github.javaparser.utils.SourceRoot;
 
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.*;
+import java.util.List;
+import java.util.regex.Pattern;
 
-public class WMCSimple {
+
+public class WMCSimple extends VoidVisitorAdapter{
     public static void main(String[] args) throws Exception {
-        String fileName = "C:\\Users\\GA\\IdeaProjects\\CS451Assignment\\weblog-analyzer\\LogAnalyzer.java";
-        FileInputStream in = new FileInputStream(fileName);
-        CompilationUnit cu = null;
-        try{
-            cu = StaticJavaParser.parse(in);
-        } catch(ParseProblemException e){
-            System.out.println("There was an error reading " + fileName);
-            System.out.println(e);
-            System.exit(1);
-        } finally {
-            in.close();
-        }
-        new MethodVisitor().visit(cu, null);
+
+//        Path pathToSource = Paths.get("C:\\Users\\GA\\IdeaProjects\\CS451Assignment");
+//        ProjectRoot pr = new ParserCollectionStrategy().collect(pathToSource);
+//        SourceRoot sourceRoot = new SourceRoot(pathToSource);
+//        sourceRoot.tryToParse();
+//        List<CompilationUnit> compilations = sourceRoot.getCompilationUnits();
+//
+//        for(CompilationUnit cu : compilations){
+//            new MethodVisitor().visit(cu, null);
+//        }
+
+        String CS451TestSystem = "C:\\Users\\GA\\IdeaProjects\\CS451Assignment\\CS451TestSystem";
+        new MethodModifier().getFiles(new File(CS451TestSystem));
     }
 
     private static class MethodVisitor extends VoidVisitorAdapter {
         public void visit(MethodDeclaration n, Object arg){
             System.out.println("Found method: " + n.getName());
         }
+    }
 
-        private int dummyMethod(){
-            return 0;
+    private static class MethodModifier extends ModifierVisitor{
+        public void getFiles(final File folder) throws FileNotFoundException {
+            for(final File entry : folder.listFiles()){
+                if (entry.isDirectory()) {
+                    getFiles(entry);
+                } else {
+                    if(entry.toString().contains(".java")) {
+                        CompilationUnit compilationUnit = StaticJavaParser.parse(entry);
+                        compilationUnit.accept(new ModifierVisitor<Void>(){
+                            public Visitable visit(BinaryExpr n,Void arg){
+                                if(n.getBegin() == "package")
+                            }
+                        }, null);
+                        new MethodVisitor().visit(compilationUnit, null);
+                    }
+                }
+            }
         }
     }
 
