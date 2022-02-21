3ab543ac9262ca046ab1d5c434b34b8918945070
GA <w.stolton@protonmail.com>
2022-02-21 19:40:08 +0000

    Completed WMC Simple/Complex and RFC. I think.

--- src/CBO.java
+++ src/CBO.java
@@ -1,24 +1,25 @@
 import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.type.VarType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.javaparser.Navigator;
-import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarators.VariableSymbolDeclarator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class CBO extends VoidVisitorAdapter{
-
-    final static String PATH = "C:\\Users\\GA\\IdeaProjects\\CS451Assignment\\CS451TestSystem";
     // Weighted Methods per Class: The number of methods in a class
+
+    //final static String PATH = "C:\\Users\\GA\\Downloads\\CS451TestSystem";
+    final static String PATH = "C:\\Users\\GA\\Downloads\\CS451TestSystem\\test";
+
     // Main, get the path of the files and call the Method Modifier
     public static void main(String[] args) throws Exception {
         new MethodModifier().analyseFiles(new File(PATH));
@@ -27,6 +28,10 @@ public class CBO extends VoidVisitorAdapter{
     private static class MethodModifier {
         // look at the location for files
         public void analyseFiles(final File folder) throws FileNotFoundException {
+
+            // Create map to store file names and references it makes to other files
+            Map<String, Set<String>> classReferences = new HashMap<>();
+
             for(final File entry : folder.listFiles()){
                 // if it's a directory, go into it
                 if (entry.isDirectory()) {
@@ -34,6 +39,7 @@ public class CBO extends VoidVisitorAdapter{
                 } else {
                     // otherwise just read each file
                     if(entry.toString().contains(".java")) {
+                        Set<String> references = new HashSet<>();
                         // instantiate number of method declarations (i.e. complexity)
                         CompilationUnit compilationUnit;
                         try {
@@ -46,26 +52,26 @@ public class CBO extends VoidVisitorAdapter{
                         compilationUnit.accept(new VoidVisitorAdapter<Void>(){
                             @Override
                             public void visit(ClassOrInterfaceDeclaration n, final Void arg) {
-                                System.out.println(n.getName());
+                                System.out.println(n.getName().toString().toUpperCase());
                                 super.visit(n, arg);
                             }
                         }, null);
 
-                        for(FieldDeclaration fieldDeclaration : compilationUnit.findAll(FieldDeclaration.class)){
-                            System.out.println(fieldDeclaration.getVariables().getParentNode());
+//                        for(FieldDeclaration fd : compilationUnit.findAll(FieldDeclaration.class)){
+//                            fd.removeComment();
+//                            System.out.println(fd);
+//                        }
+
+                        for(MethodDeclaration md : compilationUnit.findAll(MethodDeclaration.class)){
+                            System.out.println(md);
+                            System.out.println(md.findAll(VariableDeclarationExpr.class));
                         }
 
-                        TypeSolver reflectionTypeSolver = new ReflectionTypeSolver();
-                        TypeSolver javaParserTypeSolver = new JavaParserTypeSolver(new File(PATH));
 
-                        reflectionTypeSolver.setParent(reflectionTypeSolver);
 
-                        CombinedTypeSolver combinedSolver = new CombinedTypeSolver();
-                        combinedSolver.add(reflectionTypeSolver);
-                        combinedSolver.add(javaParserTypeSolver);
 
-                        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedSolver);
-                        StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
+                        // now add the file and the references to it to the map
+                        classReferences.put(entry.getName(), references);
 
                     }
                 }
