184c32ad01ec82074c351142d773ddfc8ea28e5b
GA <w.stolton@protonmail.com>
2022-03-11 15:27:36 +0000

    CBO now.

--- src/CBO.java
+++ src/CBO.java
@@ -2,19 +2,19 @@ import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.MethodReferenceExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CBO extends VoidVisitorAdapter{
     // Weighted Methods per Class: The number of methods in a class
 
-    final static String PATH = "C:\\Users\\GA\\Downloads\\CS451TestSystem\\test";
+    //final static String PATH = "C:\\Users\\GA\\Downloads\\CS451TestSystem";
+    final static String PATH = "C:\\Users\\GA\\Downloads\\test";
 
     // Main, get the path of the files and call the Method Modifier
     public static void main(String[] args) throws Exception {
@@ -36,6 +36,7 @@ public class CBO extends VoidVisitorAdapter{
                     // otherwise just read each file
                     if(entry.toString().contains(".java")) {
                         Set<String> references = new HashSet<>();
+                        final String[] className = {""};
                         // instantiate number of method declarations (i.e. complexity)
                         CompilationUnit compilationUnit;
                         try {
@@ -47,29 +48,47 @@ public class CBO extends VoidVisitorAdapter{
                         compilationUnit.accept(new VoidVisitorAdapter<Void>(){
                             @Override
                             public void visit(ClassOrInterfaceDeclaration n, final Void arg) {
-                                System.out.println(n.getName().toString().toUpperCase());
+                                className[0] = n.getName().toString();
                                 super.visit(n, arg);
                             }
                         }, null);
 
-                        for(MethodDeclaration md : compilationUnit.findAll(MethodDeclaration.class)){
-                            //TODO: here is where I need to pull SOMETHING out for references
-                            references.add(md.getNameAsString());
+                        for(VariableDeclarator ci : compilationUnit.findAll(VariableDeclarator.class)){
+                            if(!ci.getType().isPrimitiveType()){
+                                references.add(ci.getTypeAsString().replaceAll("\\[", "").replaceAll("\\]",""));
+                            }
                         }
-
                         // now add the file and the references to it to the map
-                        classReferences.put(entry.getName(), references);
+                        classReferences.put(className[0], references);
                     }
                 }
             }
-            handle(classReferences);
+            handle(removeLibraryClasses(classReferences));
         }
 
-        //TODO: here is where I need to determine the linking?
-        public void handle(Map<String, Set<String>> classesAndRefs){
-            for(Map.Entry<String, Set<String>> entry: classesAndRefs.entrySet()){
+
+        public Map<String, Set<String>> removeLibraryClasses(Map<String, Set<String>> classes){
+            Map<String, Set<String>> newClasses = new HashMap<>();
+            List<String> classList = new ArrayList<>(classes.keySet());
+            for(Map.Entry<String, Set<String>> entry : classes.entrySet()){
+                Set<String> add = new HashSet<>();
+                for(String entryString : entry.getValue()){
+                    if(classList.contains(entryString)){
+                        add.add(entryString);
+                    }
+                }
+                newClasses.put(entry.getKey(), add);
+            }
+            return newClasses;
+        }
+
+
+        //TODO: here is where I need to determine the linking
+        public void handle(Map<String, Set<String>> classReferences){
+            for(Map.Entry<String, Set<String>> entry: classReferences.entrySet()){
                 System.out.println(entry);
             }
+
         }
     }
 }
